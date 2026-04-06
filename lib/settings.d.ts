73d80c1d2bd21475b3a0a501cbb0e587717d4f3f
Tim Watson <tswatson123@gmail.com>
2026-04-06 20:00:40 +0100

    Cleanup

--- lib/settings.d.ts
+++
@@ -1,6 +0,0 @@
-import type { ServiceConnectConfig } from './types';
-/**
- * Default settings for ServiceConnect
- */
-export default function settings(): ServiceConnectConfig;
-//# sourceMappingURL=settings.d.ts.map
