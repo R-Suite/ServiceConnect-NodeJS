75b2ed439b635cc3b0e315650b8d26cc7b65f2fa
Tim Watson <tswatson123@gmail.com>
2026-05-21 22:22:21 +0100

    Add @serviceconnect/healthchecks stub package

---
+++ packages/healthchecks/tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm'],
+  dts: true,
+  clean: true,
+  target: 'node22',
+  sourcemap: true,
+  splitting: false,
+});
