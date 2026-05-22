2dd044b507804df913b936b8de683f34e65bb790
Tim Watson <tswatson123@gmail.com>
2026-05-22 08:48:07 +0100

    Add RabbitMQ-specific error subclasses (Phase C Task 2)

---
+++ packages/rabbitmq/src/errors.ts
@@ -0,0 +1,13 @@
+import { ServiceConnectError } from '@serviceconnect/core';
+
+export class RabbitMQPayloadTooLargeError extends ServiceConnectError {
+  override readonly name = 'RabbitMQPayloadTooLargeError';
+}
+
+export class RabbitMQPublishConfirmTimeoutError extends ServiceConnectError {
+  override readonly name = 'RabbitMQPublishConfirmTimeoutError';
+}
+
+export class RabbitMQTopologyMismatchError extends ServiceConnectError {
+  override readonly name = 'RabbitMQTopologyMismatchError';
+}
